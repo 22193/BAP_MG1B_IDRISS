a68fc42440a73b4d50e6f00d3100985c94fe37c2
22193 <22193@ma-web.nl>
2018-01-31 12:27:26 +0100

    Delete index.php

--- opdracht3/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-echo 'Hallo ' . $username . ' hoe gaat het?' . '<br>';
-echo 'Waarom is ' . $password . ' jou wachtwoord?';
